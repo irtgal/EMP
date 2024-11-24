088565a69c20b6bfcef4fe6ada19b1d745086894
Gal Irt <gal.irt@celtra.com>
2024-11-24 14:17:03 +0100

    Add pinia

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { createPinia } from 'pinia';
 import { Quasar } from 'quasar';
 import App from './App.vue';
 import router from './router';
+import { pinia } from './boot/pinia';
+
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(Quasar);
+app.use(pinia);
 
 app.mount('#app');
