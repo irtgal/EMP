459374b90953a8ad8e533402533792393a2566a2
Gal Irt <gal.irt@celtra.com>
2025-01-01 12:31:55 +0100

    Home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,54 @@
 <template>
     <q-page padding>
-        <div class="text-center text-h6">HOME - TODO</div>
+        <!-- Section: New Movies -->
+        <section v-if="newMovies.length" class="q-mb-xl">
+            <h1 class="text-h5 q-mb-md">New Movies</h1>
+            <MovieList :movies="newMovies" layout="row" />
+        </section>
+
+        <!-- Section: Recommended Movies -->
+        <section v-if="recommendedMovies.length" class="q-mb-xl">
+            <h1 class="text-h5 q-mb-md">Recommended Movies</h1>
+            <MovieList :movies="recommendedMovies" layout="row" />
+        </section>
+
+        <!-- Section: Rated Movies -->
+        <section v-if="ratedMovies.length" class="q-mb-xl">
+            <h1 class="text-h5 q-mb-md">Rated Movies</h1>
+            <MovieList :movies="ratedMovies" layout="row" />
+        </section>
     </q-page>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue';
+import { useMovieStore } from '../store/movieStore';
+import MovieList from 'src/components/MovieList.vue';
+
+const movieStore = useMovieStore();
+
+const newMovies = computed(() =>
+    movieStore.movies.filter((movie) => {
+        const currentYear = new Date().getFullYear();
+        return movie.releaseYear >= currentYear - 1; // Movies from the last year
+    })
+);
+
+const recommendedMovies = computed(() =>
+    movieStore.movies
+);
+
+const ratedMovies = computed(() =>
+    movieStore.movies.filter((movie) => movie.userRating != undefined)
+);
 </script>
+
+<style scoped>
+section {
+    margin-bottom: 32px;
+}
+
+h2 {
+    margin-bottom: 16px;
+}
+</style>
